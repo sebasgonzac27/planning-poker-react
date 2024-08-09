1be4ae982d4975b23f301967173c544643ade83e
Juan S Gonzalez C <juan.gonzalez@pragma.com.co>
2024-08-09 15:58:48 -0500

    feat: add useCreateForm hook for creating a party

---
+++ src/pages/create-party/hooks/use-create-form/useCreateForm.tsx
@@ -0,0 +1,39 @@
+import { validateInput } from "../../../../utils/validate-input/validate-input.ts";
+import React, { useState } from "react";
+import { createParty } from "../../../../services/party/party.ts";
+import { useNavigate } from "react-router-dom";
+import { handleError } from "../../../../utils/handle-error/handle-error.ts";
+
+export function useCreateForm() {
+  const [partyName, setPartyName] = useState<string>("");
+  const [errors, setErrors] = useState<string[]>([]);
+  const navigate = useNavigate();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPartyName(e.target.value);
+    const errors = validateInput(e.target.value);
+    if (errors) {
+      setErrors(errors);
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (errors.length > 0) {
+      handleError("Ingrese un nombre válido.");
+      return;
+    }
+
+    const { id } = await createParty(partyName);
+    if (id) {
+      navigate(`/join?partyId=${id}`);
+    }
+  };
+
+  return {
+    partyName,
+    errors,
+    handleChange,
+    handleSubmit,
+  };
+}
