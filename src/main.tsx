3d49992a56cb2dafd1bd0264d86aa52624b1982e
Juan S Gonzalez C <juan.gonzalez@pragma.com.co>
2024-08-09 15:56:12 -0500

    refactor: update react-router-dom usage in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,26 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import { Toaster } from "sonner";
+import CreateParty from "./pages/create-party/create-party";
 
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/create-party",
+    element: <CreateParty />,
+  },
+  {
+    path: "/",
+    element: <Navigate to="/create-party" />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <>
+    <RouterProvider router={router} />
+    <Toaster richColors />
+  </>
+);
