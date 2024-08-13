aeea717d4693a85254d945a5ea948f06aebbfa58
Juan S Gonzalez C <juan.gonzalez@pragma.com.co>
2024-08-13 14:47:02 -0500

    refactor: update Desk component to render Actions component within table__inner div

--- src/pages/join-party/components/playground/desk/desk.tsx
+++ src/pages/join-party/components/playground/desk/desk.tsx
@@ -1,10 +1,13 @@
+import Actions from "../actions/actions";
 import styles from "./desk.module.scss";
 
 export default function Desk() {
   return (
     <div className={styles.table}>
       <div className={styles.table__middle}>
-        <div className={styles.table__inner}></div>
+        <div className={styles.table__inner}>
+          <Actions />
+        </div>
       </div>
     </div>
   );
