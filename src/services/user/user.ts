70f559e682ff4070107914794a096654ca257ffc
Juan S Gonzalez C <juan.gonzalez@pragma.com.co>
2024-08-14 12:59:18 -0500

    feat: implement toggleRole function in user service

--- src/services/user/user.ts
+++ src/services/user/user.ts
@@ -22,3 +22,25 @@ export async function vote({
     handleError("Ocurrió un error al votar");
   }
 }
+
+export async function toggleRole({
+  role,
+  roomId,
+  userId,
+}: {
+  role: string;
+  roomId: string;
+  userId: string;
+}) {
+  try {
+    const { data } = await api.put("/party/toggle-role", {
+      role,
+      roomId,
+      userId,
+    });
+    return data;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    handleError("Ocurrió un error al cambiar de rol");
+  }
+}
